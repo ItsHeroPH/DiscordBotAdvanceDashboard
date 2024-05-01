36a316b7580be8ae920ea154853ec7bc7be99b3d
ItsHeroPH <heroreyes1973@gmail.com>
2024-05-01 18:00:24 +0800

    features

--- dashboard/src/pages/Home.jsx
+++ dashboard/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import NavigationBar from "../components/NavigationBar";
 import { useLoaderData } from "react-router-dom";
+import leveling_card from "../public/leveling-card.png"
 
 export default function Home() {
     const { bot, user } = useLoaderData()
@@ -18,6 +19,22 @@ export default function Home() {
                         <div className="flex flex-col">
                             <h1 className="text-6xl text-white font-black">{bot.username}</h1>
                             <h1 className="text-2xl text-white font-medium">The Best Multipurpose Bot</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-screen h-full bg-neutral-900">
+                <div className="w-[50%] mx-auto py-11 flex flex-col justify-center items-center gap-4">
+                    <h1 className="text-5xl text-white font-medium text-center">Features</h1>
+                    <div className="grid grid-cols-3 xl:grid-cols-6 gap-16 items-center">
+                        <div className="col-span-3 flex flex-col gap-4">
+                            <h1 className="text-4xl text-white font-black">Leveling and XP System</h1>
+                            <p className="text-md text-gray-400 font-medium text-wrap">Make it more fun in your server activities by implementing our leveling system.
+                            This gives the rewards to the most active on the server, by gaining xp and getting level ups. This is design to encourage the member of the server
+                            to actively participate to the server's activities and chats</p>
+                        </div>
+                        <div className="col-span-3">
+                            <img className="w-[400px] h-[400px]"src={leveling_card}/>
                         </div>
                     </div>
                 </div>
